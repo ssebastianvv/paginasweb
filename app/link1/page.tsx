2dc556d581974a58b60358271c89af15d48edfc5
ssebastianvv <ssebastianvapi@gmail.com>
2024-08-12 08:17:55 -0500

    n

--- app/link1/page.tsx
+++ app/link1/page.tsx
@@ -1,13 +1,9 @@
 import Image from "next/image";
-import "./styles.css";
+
 
 export default function link1() {
   return (
-    <div class name {
-      constructor(parameters) {
-        
-      }
-    }>
+    <div >
       <h1>Bienvenido a la Página 1</h1>
       
     </div>
